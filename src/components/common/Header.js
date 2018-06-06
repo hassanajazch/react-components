7df732612b7b606965731fd70b346ddb15950be7
hassanajaz <hassanajazch@gmail.com>
2018-06-06 06:19:27 +0500

    Add company logo in sidebar

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -4,14 +4,21 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className="sidenav">
-        <IndexLink to="/" activeClassName="active">- Calendar</IndexLink>
-        {" | "}
-        <Link to="/courses" activeClassName="active">- Courses</Link>
-        {" | "}
-        <Link to="/notification" activeClassName="active">- Notifications</Link>
-        {" | "}
-        <Link to="/tabs" activeClassName="active">- Tabs</Link>
+    <div>
+      <div className="sidenav">
+          <hr/>
+          <h1 className="logo-color">Inksacape</h1>
+          <hr/>
+          <div className="tablist-height">
+            <IndexLink to="/" activeClassName="active">- Calendar</IndexLink>
+            {" | "}
+            <Link to="/courses" activeClassName="active">- Courses</Link>
+            {" | "}
+            <Link to="/notification" activeClassName="active">- Notifications</Link>
+            {" | "}
+            <Link to="/tabs" activeClassName="active">- Tabs</Link>
+          </div>
+      </div>
     </div>
   );
 };
